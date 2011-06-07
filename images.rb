bf0b3cc650edf757f983d906911e389a73378beb
mikegee <gee.24@osu.edu>
2011-06-07 17:07:59 -0400

    added resizing as a hack, need to extract logic to a model

--- images.rb
+++ images.rb
@@ -2,10 +2,15 @@ require 'sinatra/base'
 
 class Images < Sinatra::Base
 
-	get '/*.*' do
-		filename = 'master/' + params[:splat].join('.')
-		halt 404 unless File.exists?(filename)
-		send_file filename
+	get '/*.*' do |path, ext|
+		path = "#{path}.#{ext}"
+		master = "master/#{path}"
+		cache = "cache/#{path}"
+		halt 404 unless File.exists?(master)
+		image = Magick::Image::read(master).first
+		image.resize!(params[:w].to_i, params[:h].to_i)
+		image.write(cache)
+		send_file cache
 	end
 
 end
